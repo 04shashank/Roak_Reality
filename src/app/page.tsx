8de6050b891b53070231c14f1d0e689c3feb1099
shashank <singh93shashank04@gmail.com>
2024-02-21 01:18:05 +0530

    again commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Roak Reality
+    </div>
+  )}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=' p-10 text-5xl font-bold border border-black text-red-400'>Header</div>
+  )
+}
+
+export default Header
