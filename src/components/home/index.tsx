fa82f94222700637203a0740de9295bab5b41130
gaurav verma <gaurav.verma@thefastway.in>
2024-03-07 16:15:33 +0530

    add slider in property card

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,4 +1,17 @@
+"use client";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+// Import Swiper styles
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+const property = {
+
+  images : [ "", "" ]
+}
 
 const dummyData = [
   {
@@ -16,21 +29,49 @@ const HomePage = () => {
   return (
     <div className="container mx-auto">
       <h2>home</h2>
-      <div className=" grid grid-cols-3 gap-4 ">
-        {dummyData.map((property, index) => {
-          return (
-            <div
-              key={index}
-              className="h-[350px] border border-gray-400 bg-black rounded-lg overflow-hidden"
-            >
-              <img
-                src={property.img}
-                alt=""
-                className=" object-cover"
-              />
-            </div>
-          );
-        })}
+      <div className=" grid grid-cols-3 gap-4 overflow-hidden ">
+        {Array(2)
+          .fill(undefined)
+          .map((property, index) => {
+            return (
+              <div>
+                <Swiper
+                  spaceBetween={50}
+                  slidesPerView={1}
+                  onSlideChange={() => console.log("slide change")}
+                  onSwiper={(swiper) => console.log(swiper)}
+                  modules={[Autoplay, Pagination]}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                  }}
+                >
+                  {dummyData.map((data, index) => {
+                    return (
+                      <SwiperSlide
+                        key={index}
+                        className="text-white border max-w-full "
+                      >
+                        <div
+                          key={index}
+                          className="h-[350px] border border-gray-400 bg-black rounded-lg overflow-hidden"
+                        >
+                          <img
+                            src={data.img}
+                            alt=""
+                            className=" object-cover bg-center h-full w-full "
+                          />
+                        </div>
+                      </SwiperSlide>
+                    );
+                  })}
+                </Swiper>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
